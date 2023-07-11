23d8585d5a29d6716314ed04e7f6df1eb83ed798
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-07-11 12:53:39 -0300

    Updating the dinamic route file
    Changed the dinamic prop file to take the type props and pass as param instead of taking it from the url like on Vanilla JS

--- app/api/todos/[id]/route.tsx
+++ app/api/todos/[id]/route.tsx
@@ -1,14 +1,18 @@
 import { NextResponse } from "next/server";
 
+type props = {
+    params: {
+        id: string
+    }
+}
+
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos"
 
-export async function GET(request: Request) {
-    const id = request.url.slice(request.url.lastIndexOf('/') + 1)
+export async function GET(request: Request, { params: { id } }: props) {
+    // const id = request.url.slice(request.url.lastIndexOf('/') + 1)
 
     const res = await fetch(`${DATA_SOURCE_URL}/${id}`)
-
     const todo: Todo = await res.json()
-
     if (!todo.id) return NextResponse.json({ "message": "To Do not found." })
 
     return NextResponse.json(todo)
